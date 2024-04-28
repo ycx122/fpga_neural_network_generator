458734139b44cc004090d6dbbf5b758476b8c57e
ycx <youchaoxing23@mails.ucas.ac.cn>
2024-04-28 19:57:44 +0800

    new

--- test2.py
+++ test2.py
@@ -12,6 +12,26 @@ class FullyConnectedNetworkGenerator:
         }
         self.layers.append(layer_params)
 
+    def add_fc_layer(self, input_width, input_num, output_width, output_num):
+        layer_params = {
+            'input_width': input_width,
+            'input_num': input_num,
+            'output_width': output_width,
+            'output_num': output_num,
+            "layer_code":'fc'
+        }
+        self.layers.append(layer_params)
+
+    def add_relu_layer(self, input_width, input_num):
+        layer_params = {
+            'input_width': input_width,
+            'input_num': input_num,
+            'output_width': input_width,
+            'output_num': input_num,
+            "layer_code":'relu'
+        }
+        self.layers.append(layer_params)   
+
     def generate_fc_instance(self, layer_params, instance_name, prev_instance_name, next_instance_name, weight_file):
         input_wire = f"data_out_{prev_instance_name}" if prev_instance_name else "data_in_top"
         output_wire = f"data_out_{instance_name}" if next_instance_name else "data_out_top"
@@ -34,15 +54,13 @@ class FullyConnectedNetworkGenerator:
         );
         """
 
-    def generate_relu_instance(self, layer_params, instance_name, prev_instance_name, next_instance_name, weight_file):
+    def generate_relu_instance(self, layer_params, instance_name, prev_instance_name, next_instance_name):
         input_wire = f"data_out_{prev_instance_name}" if prev_instance_name else "data_in_top"
         output_wire = f"data_out_{instance_name}" if next_instance_name else "data_out_top"
 
         return f"""relu #(
             .INPUT_WIDTH({layer_params['input_width']}),
-            .INPUT_NUM({layer_params['input_num']}),
-            .OUTPUT_WIDTH({layer_params['output_width']}),
-            .OUTPUT_NUM({layer_params['output_num']})
+            .INPUT_NUM({layer_params['input_num']})
             )
 
         ) relu_{instance_name} (
@@ -75,7 +93,7 @@ class FullyConnectedNetworkGenerator:
                 prev_instance_name = f"layer{i}" if i > 0 else ""
                 next_instance_name = f"layer{i+2}" if i < len(self.layers) - 1 else ""
             
-                instances.append(self.generate_relu_instance(params, f"layer{i+1}", prev_instance_name, next_instance_name, weight_file))
+                instances.append(self.generate_relu_instance(params, f"layer{i+1}", prev_instance_name, next_instance_name))
 
             if i < len(self.layers) - 1:  # Add intermediate wires between layers
                 wire_width = params['output_width'] * params['output_num']
@@ -96,7 +114,7 @@ module top_network(
 // Intermediate wires
 {"".join(wires)}
 
-// Instances of fully connected layers
+// Instances of connected layers
 {"".join(instances)}
 
 endmodule
@@ -105,10 +123,12 @@ endmodule
 
 # Usage
 network_generator = FullyConnectedNetworkGenerator()
-network_generator.add_layer(8, 32, 16, 64,"fc")   # Adding first layer
-network_generator.add_layer(16, 64, 16, 64,"relu")   # Adding first layer
-network_generator.add_layer(16, 64, 8, 10,"fc")   # Adding second layer
-network_generator.add_layer(8, 10, 8, 10,"relu")   # Adding first layer
+network_generator.add_fc_layer(8, 32, 16, 64)   # Adding first layer
+network_generator.add_relu_layer(16, 64)   # Adding first layer
+network_generator.add_fc_layer(16, 64, 8, 10)   # Adding second layer
+network_generator.add_relu_layer(8, 10)   # Adding first layer
 verilog_code = network_generator.generate_top_module()
+
+print(network_generator.layers)
 
 print(verilog_code)
